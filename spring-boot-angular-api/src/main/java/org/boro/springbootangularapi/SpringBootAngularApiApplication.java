91b279ab7a7237d85bd24b5c262e784db675e5dc
Artur Borodziej <artur.borodziej@tietoevry.com>
2023-10-05 16:39:32 +0200

    Spring Boot Angular Demo initial commit

---
+++ spring-boot-angular-api/src/main/java/org/boro/springbootangularapi/SpringBootAngularApiApplication.java
@@ -0,0 +1,13 @@
+package org.boro.springbootangularapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-angular-api/src/test/java/org/boro/springbootangularapi/SpringBootAngularApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boro.springbootangularapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
